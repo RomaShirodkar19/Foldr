6b0fdd298215a2e2dcf28af429abe6d8615f3a0f
RomaShirodkar19 <romashirodkar19@gmail.com>
2026-06-14 22:01:40 +0530

    Add Project

---
+++ foldrwebapp/src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Foldr Web App</h1>
+      {/* Add your components and routes here */}
+    </div>
+  );
+}
+
+export default App;
