37f45ce751b7077be0614e2dbf54df34cf9a0a86
RomaShirodkar19 <romashirodkar19@gmail.com>
2026-07-24 23:31:27 +0530

    Complete custom security filter chain

---
+++ foldrapi/src/main/java/com/roma/foldrapi/security/ClerkJwtAuthFilter.java
@@ -0,0 +1,89 @@
+package com.roma.foldrapi.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.security.PublicKey;
+import java.util.Base64;
+import java.util.Collections;
+
+@Component
+@RequiredArgsConstructor
+public class ClerkJwtAuthFilter extends OncePerRequestFilter {
+
+    @Value("${clerk.issuer}")
+    private String clerkIssuer;
+
+    private ClerkJwksProvider jwksProvider;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        if(request.getRequestURI().contains("/webhooks")) {
+            filterChain.doFilter(request, response);
+        }
+
+        String authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Missing or invalid Authorization header");
+            return;
+        }
+
+        try {
+            String token = authHeader.substring(7);
+            String[] chunks = token.split("\\.");
+            if(chunks.length < 3) {
+                response.sendError(HttpServletResponse.SC_FORBIDDEN, "Invalid JWT token format");
+                return;
+            }
+
+            String headerJson = new String(Base64.getDecoder().decode(chunks[0])); // Decode header
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode headerNode = objectMapper.readTree(headerJson);
+
+            if(!headerNode.has("kid")) {
+                response.sendError(HttpServletResponse.SC_FORBIDDEN, "JWT token missing 'kid' in header");
+                return;
+            }
+
+            String kid = headerNode.get("kid").asText();
+            PublicKey publicKey = jwksProvider.getPublicKey(kid);// This will throw an exception if the key is not found or invalid
+
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(publicKey)
+                    .setAllowedClockSkewSeconds(60)
+                    .requireIssuer(clerkIssuer)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            String clerkId = claims.getSubject();
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(clerkId, null, Collections.singletonList(new SimpleGrantedAuthority("ROLE_ADMIN")));
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+            filterChain.doFilter(request, response);
+
+        } catch (Exception e ) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Invalid JWT token: " + e.getMessage());
+            return;
+        }
+
+    }
+}
